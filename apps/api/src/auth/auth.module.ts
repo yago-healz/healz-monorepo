198e15bc526c169f7383c6b3269c5d1bc73d969c
Yago Healz <yago@healz.com.br>
2026-02-07 12:27:54 -0300

    Remove DatabaseModule import from AuthModule to streamline dependencies

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,14 +1,12 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
-import { DatabaseModule } from "../database/database.";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtStrategy } from "./strategies/jwt.strategy";
 
 @Module({
   imports: [
-    DatabaseModule,
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
